6295d7a9748c67922e044bc75c8ba5fdd74ddc29
odomin2 <odomin2@students.towson.edu>
2016-03-04 14:46:28 -0500

    Task 12 Automatically Generating Documentation.
    Added Javadocs documentation to the project using JAutodoc plug in and
    generating Javadoc to the project.  Ran JUnit test and no errors came
    up.

---
+++ Task 12 Auto Gener Doc/Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/RailRoadCellTest.java
@@ -0,0 +1,76 @@
+package edu.towson.cis.cosc603.project2.monopoly;
+
+import junit.framework.TestCase;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class RailRoadCellTest.
+ */
+public class RailRoadCellTest extends TestCase {
+	
+	/** The game master. */
+	GameMaster gameMaster;
+	
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() {
+		gameMaster = GameMaster.instance();
+		gameMaster.setGameBoard(new GameBoardRailRoad());
+		gameMaster.setNumberOfPlayers(2);
+		gameMaster.reset();
+		gameMaster.setGUI(new MockGUI());
+	}
+	
+	/**
+	 * Test player action.
+	 */
+	public void testPlayerAction() {
+		RailRoadCell cell =
+			(RailRoadCell) gameMaster.getGameBoard().queryCell("Railroad A");
+		int cellIndex = gameMaster.getGameBoard().queryCellIndex("Railroad A");
+		gameMaster.movePlayer(0, cellIndex);
+		gameMaster.getPlayer(0).purchase();
+		gameMaster.switchTurn();
+		gameMaster.movePlayer(1, cellIndex);
+		cell.playAction(null);
+		assertEquals(
+				1500 - cell.getRent(),
+				gameMaster.getPlayer(1).getMoney());
+		assertEquals(
+				1300 + cell.getRent(),
+				gameMaster.getPlayer(0).getMoney());
+	}
+	
+	/**
+	 * Test purchase railroad.
+	 */
+	public void testPurchaseRailroad() {
+		assertEquals(0, gameMaster.getPlayer(0).numberOfRR());
+		int cellIndex = gameMaster.getGameBoard().queryCellIndex("Railroad A");
+		gameMaster.movePlayer(0, cellIndex);
+		gameMaster.getPlayer(0).purchase();
+		assertEquals(1300, gameMaster.getPlayer(0).getMoney());
+		assertEquals(1, gameMaster.getPlayer(0).numberOfRR());
+	}
+
+	/**
+	 * Test rent.
+	 */
+	public void testRent() {
+		RailRoadCell rr1 =
+			(RailRoadCell) gameMaster.getGameBoard().queryCell("Railroad A");
+		int cellIndex1 = gameMaster.getGameBoard().queryCellIndex("Railroad A");
+		gameMaster.movePlayer(0, cellIndex1);
+		gameMaster.getPlayer(0).purchase();
+		assertEquals(25, rr1.getRent());
+
+		RailRoadCell rr2 =
+			(RailRoadCell) gameMaster.getGameBoard().queryCell("Railroad B");
+		int cellIndex2 = gameMaster.getGameBoard().queryCellIndex("Railroad B");
+		gameMaster.movePlayer(0, cellIndex2 - cellIndex1);
+		gameMaster.getPlayer(0).purchase();
+		assertEquals(50, rr1.getRent());
+		assertEquals(50, rr2.getRent());
+	}
+}
---
+++ Task 12 Auto Gener Doc/Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/TradeDealTest.java
@@ -0,0 +1,36 @@
+package edu.towson.cis.cosc603.project2.monopoly;
+
+import junit.framework.TestCase;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class TradeDealTest.
+ */
+public class TradeDealTest extends TestCase {
+    
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    public void setUp() {
+        GameMaster gameMaster = GameMaster.instance();
+        gameMaster.reset();
+        gameMaster.setNumberOfPlayers(2);
+        gameMaster.getPlayer(0).setName("Buyer");
+        gameMaster.getPlayer(1).setName("Seller");
+    }
+
+    /**
+     * Test make message.
+     */
+    public void testMakeMessage() {
+        TradeDeal deal = new TradeDeal();
+        deal.setAmount(200);
+        deal.setPropertyName("Blue 1");
+        deal.setSellerIndex(1);
+        GameMaster.instance().getPlayer(0);
+        String message = "Buyer wishes to purchase Blue 1 from Seller" +
+        		" for 200.  Seller, do you wish to trade your property?";
+        assertEquals(message, deal.makeMessage());
+    }
+
+}
